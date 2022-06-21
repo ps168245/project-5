f02c854ae272ff33c7e5a79fdfd9558d090d6bd5
ps168245 <ps168245@edu.summacollege.nl>
2022-06-21 15:23:45 +0200

    nav added and two components (home & about us)

--- [USERINTERFACE] Laravel/SummaMove/vendor/composer/autoload_real.php
+++ [USERINTERFACE] Laravel/SummaMove/vendor/composer/autoload_real.php
@@ -25,38 +25,15 @@ class ComposerAutoloaderInit870de1664d785478885eb16668d365cd
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit870de1664d785478885eb16668d365cd', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit870de1664d785478885eb16668d365cd', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit870de1664d785478885eb16668d365cd::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit870de1664d785478885eb16668d365cd::getInitializer($loader));
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit870de1664d785478885eb16668d365cd::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = \Composer\Autoload\ComposerStaticInit870de1664d785478885eb16668d365cd::$files;
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire870de1664d785478885eb16668d365cd($fileIdentifier, $file);
         }
---
+++ [USERINTERFACE] React Native/SummaMove/Components/exerciseScreen.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, FlatList, Image, url, ImageBackground,WebFont } from 'react-native';
+import React, { Component } from 'react';
+
+
+const exerciseScreen = () => {
+    return (
+  <View style={styles.container}>
+  
+    </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'cyan',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+});
+
+export default exerciseScreen
