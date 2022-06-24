78148382fad31cfe4b27f8471943874a305be343
PS220273 <ps220273@edu.summacollege.nl>
2022-06-24 13:18:55 +0200

    api beveligd met token

---
+++ Laravel/SummaMove/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon as carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "Jan Jansen",
+            'email' => "JJ@gmail.com",
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make("password"),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
